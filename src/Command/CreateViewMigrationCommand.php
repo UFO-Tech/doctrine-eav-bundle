0a253407294a7e3d76df2a6b4fa41ddd5f3bdbe5
Ashterix <ashterix69@gmail.com>
2024-05-27 19:04:33 +0300

    fix

--- src/Command/CreateViewMigrationCommand.php
+++ src/Command/CreateViewMigrationCommand.php
@@ -28,7 +28,7 @@ class CreateViewMigrationCommand extends Command
         ;
     }
 
-    protected function execute(InputInterface $input, OutputInterface $output)
+    protected function execute(InputInterface $input, OutputInterface $output): int
     {
         $io = new SymfonyStyle($input, $output);
         $migrationName = shell_exec('php bin/console doctrine:migrations:generate');
